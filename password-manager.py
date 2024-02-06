504f5083eaaa5ea0bc300b3f54423b5571ac7d0d
Alivia Ross <Alivia.ross9@gmail.com>
2024-02-06 02:03:48 -0500

    Formatting changes

--- password-manager.py
+++ password-manager.py
@@ -1,7 +1,5 @@
-import random
 import secrets
 import string
-
 
 listAll = string.ascii_letters + string.digits
 
@@ -15,14 +13,15 @@ def inputLength():  # function which allows the user to input their desired pass
     global length
     try:
         length = int(input("How many characters should your password be? "))  # Asks user for password length
-    except:  # Error Handling; This is a broad except statement which needs to be further tested to ensure invalid number is the issue
+    except:
+        # Error Handling; Needs to be further tested to ensure invalid number is [always] the issue
         print("Please enter a valid number")
         inputLength()
     if length < 10:  # checks for  a length greater than 10 (as specified in the assignment instructions)
         print("Your password must be at least 10 characters long!")
         inputLength()
     else:
-        GeneratePassword(length)  # proceeds to the generate password funtion with the user inputed length
+        GeneratePassword(length)  # proceeds to the generate password funtion with the user inputted length
 
 
 def ContainsTwoNumbers(password, length):  # verifies the password generated contains two numbers
@@ -36,6 +35,7 @@ def ContainsTwoNumbers(password, length):  # verifies the password generated con
     else:
         print("Generated password:", ''.join(password))  # prints the generated password
         SelectionScreen(password)
+
 
 def SelectionScreen(password):
     choice = int(input("Would you like to \n 1. Generate Password \n 2. Encrypt \n 3. Decrypt \n 4. Close"))
@@ -52,37 +52,43 @@ def SelectionScreen(password):
     else:
         print("Please select a valid option")
 
+
 def encrypt(password):
     encryptPass = ''
     for i in password:
-        if i.isalpha(): # Encrypt letters
+        if i.isalpha():  # Encrypt letters
             if i.isupper():
-                encryptPass += chr((ord(i) + 3 - ord('A')) % 26 + ord('A')) #shifts the character; mod. 26 ensures it stays in the alphabet
+                # shifts the character; mod. 26 ensures it stays in the alphabet
+                encryptPass += chr((ord(i) + 3 - ord('A')) % 26 + ord('A'))
             else:
                 encryptPass += chr((ord(i) + 3 - ord('a')) % 26 + ord('a'))
-        elif i.isdigit(): # Encrypt digits
-            encryptPass += chr((ord(i) + 3 - ord('0')) % 10 + ord('0')) #shifts the character; mod. 10 ensures it stays in the numbers
+        elif i.isdigit():  # Encrypt digits
+            # shifts the character; mod. 10 ensures it stays in the numbers
+            encryptPass += chr((ord(i) + 3 - ord('0')) % 10 + ord('0'))
     print("Original text:", password)
     print("Encrypted text:", encryptPass)
     SelectionScreen("none")
 
+
 def bruteForceDecrypt(input_text):
     decrypted_result = ''
     for shift in range(1, 26):
-        for char in input_text:
-            if char.isalpha():
-                if char.isupper():
-                    decrypted_result += chr((ord(char) - shift - ord('A')) % 26 + ord('A'))
+        for i in input_text:
+            if i.isalpha():
+                if i.isupper():
+                    decrypted_result += chr((ord(i) - shift - ord('A')) % 26 + ord('A'))
                 else:
-                    decrypted_result += chr((ord(char) - shift - ord('a')) % 26 + ord('a'))
-            elif char.isdigit():
-                decrypted_result += chr((ord(char) - shift - ord('0')) % 10 + ord('0'))
+                    decrypted_result += chr((ord(i) - shift - ord('a')) % 26 + ord('a'))
+            elif i.isdigit():
+                decrypted_result += chr((ord(i) - shift - ord('0')) % 10 + ord('0'))
             else:
-                decrypted_result += char
+                decrypted_result += i
 
         print("Shift +" + str(shift) + ": " + decrypted_result)
         decrypted_result = ''
         SelectionScreen("none")
+
+
 def decrypt():
     encrypted_text = input("Enter encrypted text")
     shift = int(input("Enter the shift (Enter 0 to brute force) "))
